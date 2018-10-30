5f859db34705a4d8ae1093af215fb50606bb0690
Alejandro García Miravet <alejandro.garcia-miravet@sogeti.com>
2018-10-30 06:59:56 +0100

    +Add function to create work items of type task

--- azuredevopsclient.go
+++ azuredevopsclient.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
 
 //Client for manage azure devops organization
@@ -17,6 +18,89 @@ type ProjectResponse struct {
 	ID     string `json:"id"`
 	Status string `json:"status"`
 	URL    string `json:"url"`
+}
+
+type WorkItemResponse struct {
+	ID     int `json:"id"`
+	Rev    int `json:"rev"`
+	Fields struct {
+		SystemAreaPath                     string    `json:"System.AreaPath"`
+		SystemTeamProject                  string    `json:"System.TeamProject"`
+		SystemIterationPath                string    `json:"System.IterationPath"`
+		SystemWorkItemType                 string    `json:"System.WorkItemType"`
+		SystemState                        string    `json:"System.State"`
+		SystemReason                       string    `json:"System.Reason"`
+		SystemCreatedDate                  time.Time `json:"System.CreatedDate"`
+		SystemCreatedBy                    string    `json:"System.CreatedBy"`
+		SystemChangedDate                  time.Time `json:"System.ChangedDate"`
+		SystemChangedBy                    string    `json:"System.ChangedBy"`
+		SystemCommentCount                 int       `json:"System.CommentCount"`
+		SystemTitle                        string    `json:"System.Title"`
+		MicrosoftVSTSCommonStateChangeDate time.Time `json:"Microsoft.VSTS.Common.StateChangeDate"`
+		MicrosoftVSTSCommonPriority        int       `json:"Microsoft.VSTS.Common.Priority"`
+	} `json:"fields"`
+	Links struct {
+		Self struct {
+			Href string `json:"href"`
+		} `json:"self"`
+		WorkItemUpdates struct {
+			Href string `json:"href"`
+		} `json:"workItemUpdates"`
+		WorkItemRevisions struct {
+			Href string `json:"href"`
+		} `json:"workItemRevisions"`
+		WorkItemHistory struct {
+			Href string `json:"href"`
+		} `json:"workItemHistory"`
+		HTML struct {
+			Href string `json:"href"`
+		} `json:"html"`
+		WorkItemType struct {
+			Href string `json:"href"`
+		} `json:"workItemType"`
+		Fields struct {
+			Href string `json:"href"`
+		} `json:"fields"`
+	} `json:"_links"`
+	URL string `json:"url"`
+}
+
+func CreateWorkItem(pat string, organization string, projectName string, title string) WorkItemResponse {
+
+	var jsonRequest = "[ { \"op\": \"add\", \"path\": \"/fields/System.Title\", \"from\": null,\"value\": \"" + title + "\"}]"
+
+	var jsonStr = []byte(jsonRequest)
+
+	var baseURL = "https://dev.azure.com/" + organization + "/" + projectName + "/_apis/wit/workitems/$Task?api-version=4.1"
+
+	req, err := http.NewRequest("POST", baseURL, bytes.NewBuffer(jsonStr))
+
+	basic := "Basic " + pat
+
+	req.Header.Set("Authorization", basic)
+	req.Header.Set("Content-Type", "application/json-patch+json")
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+
+	responseData, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(responseData))
+
+	data := WorkItemResponse{}
+	json.Unmarshal([]byte(responseData), &data)
+
+	return data
+
 }
 
 func CreateProject(pat string, organization string, projectName string) ProjectResponse {
